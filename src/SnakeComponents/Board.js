d33d83f427e26e4212092e0e0aea9a7848a912af
Minh Phan <minhpq.contact@gmail>
2019-10-03 09:37:23 +0700

    Update auto move snake

--- src/SnakeComponents/Board.js
+++ src/SnakeComponents/Board.js
@@ -4,13 +4,15 @@ import './Board.css';
 
 class Board extends Component {
 
+  speed = 1000;
 
   constructor(props) {
     super(props);
     let size =  this.props.size;
     this.state = {
       matrix: [],
-      snake: []
+      snake: [],
+      direct: 'left'
     }
     this.state.snake.push({
       x: Math.floor(size/2),
@@ -22,53 +24,74 @@ class Board extends Component {
         let check = this.isSnake(i,j)
         squareStates.push(check);
       }
-      // this.state.matrix.push(items);
       this.state.matrix.push(squareStates);
     }
   }
 
   updateMatrix = (coord) => {
     let matrix = this.state.matrix;
-    matrix[coord.x][coord.y] = true
-    this.setState({
-      matrix: matrix
-    })
+    if(matrix[coord.x][coord.y]) {
+      clearInterval(this.snakeMoving);
+      alert('Lose!');
+    } else {
+      matrix[coord.x][coord.y] = true
+      this.setState({
+        matrix: matrix
+      })
+    }
+  }
+
+  snakeMoving = () => {
+    let bx = 0;
+    let by = 0;
+    switch(this.state.direct) {
+      case 'left':
+        bx = 0;
+        by = -1;
+        break;
+      case 'up':
+        bx = -1;
+        by = 0;
+        break;
+      case 'right':
+        bx = 0;
+        by = 1;
+        break;
+      case 'down':
+        bx = 1;
+        by = 0;
+        break;
+    }
+    let tmpSnake = this.state.snake;
+    let head = tmpSnake.pop();
+    let nextMoveX = (head.x + bx) >= this.props.size || (head.x + bx) < 0 ? head.x:head.x+bx;
+    let nextMoveY = (head.y + by) >= this.props.size || (head.y + by) < 0 ? head.y:head.y+by;
+
+    let mover = {
+      x: nextMoveX,
+      y: nextMoveY
+    };
+    this.updateMatrix(mover)
+    tmpSnake.push(mover);
+    this.setState({snake: tmpSnake});
+
   }
 
   updateSnake = (ev) => {
     if(ev){
-      let bx = 0;
-      let by = 0;
-      if(ev.keyCode === 38) {
-        //left
-        bx = -1;
-        by = 0;
-      } else if (ev.keyCode === 37) {
+      if(ev.keyCode === 38 && this.state.direct != 'down') {
         //up
-        bx = 0;
-        by = -1;
-      } else if (ev.keyCode === 40) {
-        //right
-        bx = 1;
-        by = 0;
-      } else if (ev.keyCode === 39) {
+        this.setState({direct: 'up'})
+      } else if (ev.keyCode === 37 && this.state.direct != 'right') {
+        //left
+        this.setState({direct: 'left'})
+      } else if (ev.keyCode === 40 && this.state.direct != 'up') {
         //down
-        bx = 0;
-        by = 1;
+        this.setState({direct: 'down'})
+      } else if (ev.keyCode === 39 && this.state.direct != 'left') {
+        //right
+        this.setState({direct: 'right'})
       }
-      let tmpSnake = this.state.snake;
-      let head = tmpSnake.pop();
-      let nextMoveX = (head.x + bx) >= this.props.size || (head.x + bx) < 0 ? head.x:head.x+bx;
-      let nextMoveY = (head.y + by) >= this.props.size || (head.y + by) < 0 ? head.y:head.y+by;
-      let mover = {
-        x: nextMoveX,
-        y: nextMoveY
-      };
-      tmpSnake.pop();
-      tmpSnake.push(mover);
-      this.setState({snake: tmpSnake});
-      this.updateMatrix(mover)
-      console.log(mover)
     }
     ev.preventDefault()
   };
@@ -85,6 +108,10 @@ class Board extends Component {
       }
     })
     return rs
+  }
+
+  componentDidMount() {
+    setInterval(this.snakeMoving, 1000);
   }
 
   render() {
